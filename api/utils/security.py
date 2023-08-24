49f12e40d24c022dda2eec0363ddd1b3735d99ac
Van Lam Nguyen <vanlam1412@gmail.com>
2023-08-24 16:46:29 +0700

    update

--- api/utils/security.py
+++ api/utils/security.py
@@ -1,10 +1,10 @@
 from fastapi import Depends, Header, HTTPException
 
 
-def get_token_authorization(TokenNguyenVanLam: str = Header(..., description="Nhập token NguyenVanLam")) -> str:
-    if TokenNguyenVanLam != "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c":
+def get_token_authorization(Token: str = Header(..., description="Nhập token")) -> str:
+    if Token != "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c":
         raise HTTPException(status_code=401, detail="Unauthorized")
-    return TokenNguyenVanLam
+    return Token
 
 
 
