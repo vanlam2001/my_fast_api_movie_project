02d90b1de1118539536aeef3c41d26e25ef51d08
vanlam <vanlam1412@gmail.com>
2023-09-09 02:00:47 +0700

    update

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://admin-movie-project-fastapi-6jndrflm6-vanlam2001.vercel.app"
+        "https://admin-movie-project-fastapi-6jndrflm6-vanlam2001.vercel.app",
+        "https://admin-movie-project-fastapi.vercel.app"
         ],  # Thay bằng địa chỉ của ứng dụng React của bạn
     allow_credentials=True,
     allow_methods=["*"],
